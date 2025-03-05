6dfda199778043619b6cf1ce51e057e0f4cd4dcc
Muqaddas Shaikh <168252812+Muqaddas2228@users.noreply.github.com>
2025-03-05 20:37:47 +0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Set Page Title
+st.set_page_config(page_title="Python Message", page_icon="🐍")
+
+# Main UI
+st.title("📢 Hello, World! 🌍")
+st.subheader("Welcome to Python Programming. Let's start coding! 🚀")
+
+# Styled Info Box
+st.success("💡 Need help? Feel free to contact the admin.")
+
+# Footer
+st.markdown("---")
+st.caption("Powered by Streamlit made by muqaddas🚀")
